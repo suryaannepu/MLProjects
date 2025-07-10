a6e07fac278e292119323d3efeb06a80bb00d3fe
suryaannepu <suryaannepu922@gmail.com>
2025-07-10 08:10:16 +0530

    model training done

--- src/utils.py
+++ src/utils.py
@@ -5,15 +5,39 @@ import numpy as np
 import pandas as pd
 
 from src.exception import CustomException
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            # Train the model
+            model.fit(X_train, y_train)
+
+            # Predict on train and test data
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Evaluate performance
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
